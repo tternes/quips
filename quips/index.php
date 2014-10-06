a798c3076a319113892b72f7bd35406c739349ac
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-10-06 12:06:38 -0500

    Add basic auth support, placeholder settings

--- quips/index.php
+++ quips/index.php
@@ -8,7 +8,18 @@ function load_settings()
         "url" => "http://th.adde.us/quips/feed.rss",
         "description" => "stuff",
         "language" => "en-us",
-        "copyright" => "Copyright (C) 2014 Thaddeus Ternes"
+        "copyright" => "Copyright (C) 2014 Thaddeus Ternes",
+
+        "basicauth" => [
+            // list of user/password pairs
+            "thaddeus" => "password",
+        ],
+        
+        "paths" => [
+            "posts" => "/tmp/quips",
+        ],
+        
+        "pretty" => true,
     ];
 }
 
@@ -41,24 +52,94 @@ function generate_rss_document()
     }
 
     // flat output
-    // echo $rss->asXml();
-    
-    // pretty output
-    $dom = dom_import_simplexml($rss)->ownerDocument;
-    $dom->formatOutput = true;
-    echo $dom->saveXML();
+    $pretty = $feed_settings["pretty"];
+    if($pretty || isset($_GET["pretty"]))
+    {
+        $dom = dom_import_simplexml($rss)->ownerDocument;
+        $dom->formatOutput = true;
+        echo $dom->saveXML();
+    }
+    else
+    {
+        echo $rss->asXml();
+    }
 }
 
+function generate_basicauth_response()
+{
+    header('WWW-Authenticate: Basic realm="Quips"');
+    header('HTTP/1.0 401 Unauthorized');
+}
+
+function create_post_from_current_request()
+{
+    $body = @file_get_contents('php://input');
+    $post = json_decode($body);
+    print_r($post);
+}
+
+function verify_authentication()
+{
+    // verify authentication
+    $authenticated = false;
+    if(!isset($_SERVER["PHP_AUTH_USER"]) || !isset($_SERVER["PHP_AUTH_PW"]))
+    {
+        $authenticated = false;
+    }
+    else
+    {
+        // verify credentials from settings
+        $authenticated = false;
+        
+        $request_user = $_SERVER["PHP_AUTH_USER"];
+        $request_pass = $_SERVER["PHP_AUTH_PW"];
+
+        $users = $GLOBALS["feed_settings"]["basicauth"];
+        foreach($users as $username => $password)
+        {
+            if($username === $request_user && $password === $request_pass)
+            {
+                $authenticated = true;
+                break;
+            }
+        }
+    }
+    
+    return $authenticated;
+}
+
+// ------------------------------------------------------------------------------------
+// Setup
+// ------------------------------------------------------------------------------------
+// enable output buffering, since we fiddle with headers in here
+ob_start();
+
+
+// ------------------------------------------------------------------------------------
+// Run
+// ------------------------------------------------------------------------------------
 if(php_sapi_name() == "cli")
 {
     // command line
     generate_rss_document();
 }
-else if($_REQUEST["METHOD"] == "POST")
-{
-    // do post
+else if($_SERVER["REQUEST_METHOD"] == "POST")
+{    
+
+    load_settings();
+    $authenticated = verify_authentication();
+    if($authenticated)
+    {
+        create_post_from_current_request();
+        print("Post Created\n"); // todo
+    }
+    else
+    {
+        generate_basicauth_response();        
+        print("Authentication Required\n");
+    }
 }
-else if($_REQUEST["METHOD"] == "GET")
+else if($_SERVER["REQUEST_METHOD"] == "GET")
 {
     generate_rss_document();
 }
