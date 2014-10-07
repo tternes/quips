dc14ad7be1c13512f42913ab9c7d38786bca4c08
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-10-06 21:17:29 -0500

    Load posts from the data directory

--- quips/index.php
+++ quips/index.php
@@ -2,31 +2,55 @@
 
 function load_settings()
 {
-    // TODO: load from external file
     $GLOBALS["feed_settings"] = [
+        
+        // RSS feed settings - map to <channel> child elements
         "title" => "Feed Name",
-        "url" => "http://th.adde.us/quips/feed.rss",
+        "link" => "http://th.adde.us/quips?format=rss",
         "description" => "stuff",
         "language" => "en-us",
         "copyright" => "Copyright (C) 2014 Thaddeus Ternes",
+        
+        // timezone to use for pubDate values
+        "timezone" => "America/Chicago",
 
         "basicauth" => [
             // list of user/password pairs
             "thaddeus" => "password",
         ],
         
+        // data directories for the app
         "paths" => [
+            
+            // data directory (text files) for posts
             "posts" => "/tmp/quips",
         ],
         
-        "pretty" => true,
+        // number of posts to appear in the RSS feed
+        "post_limit_count" => 10,
+        
+        // development values - use false for both in production
+        "pretty" => false,
+        "debug" => false,
     ];
+    
+    if(file_exists('settings.inc.php'))
+        require_once('settings.inc.php');
 }
 
+function is_debug()
+{
+    global $feed_settings;
+    $result = false;
+
+    if(isset($feed_settings["debug"]) && $feed_settings["debug"] == true)
+        $result = true;
+    
+    return $result;
+}
 
 function generate_rss_document()
 {
-    load_settings();
     header("Content-Type: application/rss+xml");
     
     $rss = new SimpleXMLElement('<rss/>');
@@ -41,14 +65,23 @@ function generate_rss_document()
     $channel->AddChild("language", $feed_settings["language"]);
     $channel->AddChild("copyright", $feed_settings["copyright"]);
 
-    // TODO: load real data
-    for($i=0; $i<10; $i++)
+    $path_posts = $feed_settings["paths"]["posts"];
+    $post_limit_count = $feed_settings["post_limit_count"];
+    $files = array_slice(array_diff(scandir($path_posts, SCANDIR_SORT_DESCENDING), array('..', '.')), 0, $post_limit_count);
+    foreach($files as $file)
     {
+        $fullpath = $path_posts . "/" . $file;
+        $post_contents = file_get_contents($fullpath);
+        $post = json_decode($post_contents);
+
         $item = $channel->AddChild("item");
-        $item->AddChild("title", ""); // no title in Snippets
-        $item->AddChild("description", "little thing");
+        $item->AddChild("title", $post->title);
+        $item->AddChild("description", $post->description);
+        $item->AddChild("pubDate", $post->date);
         $item->AddChild("link");
-        // $item->AddChild("pubDate", date("D, d M Y H:i:s O", strtotime($date)));
+        
+        if(is_debug())
+            $item->AddChild("path", $fullpath);
     }
 
     // flat output
@@ -73,9 +106,38 @@ function generate_basicauth_response()
 
 function create_post_from_current_request()
 {
+    global $feed_settings;
+
     $body = @file_get_contents('php://input');
+    
+    // if the post is plain text, create a post object
+    // or, if the post is JSON, store it verbatim
     $post = json_decode($body);
+    
+    if($post == null)
+    {
+        $post = [
+            "description" => $body,
+        ];
+    }
+    
+    // make sure there's a date
+    if(!isset($post->date))
+    {
+        // pubDate format
+        $post["date"] = date("D, d M Y H:i:s O");
+    }
+    
     print_r($post);
+    
+    // write json file to /path/to/posts/YYYYMMDDhhmm.txt
+    $path_posts = $feed_settings["paths"]["posts"];
+    $fullpath = $path_posts . "/" . date("YmdHis") . ".txt";
+
+    print_r($fullpath);
+    $fp = fopen($fullpath, "w") or die("unable to open output file");
+    fwrite($fp, json_encode($post));
+    fclose($fp);
 }
 
 function verify_authentication()
@@ -114,6 +176,11 @@ function verify_authentication()
 // enable output buffering, since we fiddle with headers in here
 ob_start();
 
+load_settings();
+
+global $feed_settings;
+isset($feed_settings["timezone"]) or die("timezone must be specified");
+date_default_timezone_set($feed_settings["timezone"]) or die("timezone is invalid");
 
 // ------------------------------------------------------------------------------------
 // Run
@@ -121,12 +188,13 @@ ob_start();
 if(php_sapi_name() == "cli")
 {
     // command line
+    global $feed_settings;
+    $feed_settings["debug"] = true;
+    $feed_settings["post_limit_count"] = 2;
     generate_rss_document();
 }
 else if($_SERVER["REQUEST_METHOD"] == "POST")
-{    
-
-    load_settings();
+{
     $authenticated = verify_authentication();
     if($authenticated)
     {
